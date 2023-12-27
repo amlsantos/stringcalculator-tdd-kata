f8dc59bbe9d701ac3831fb54390d9bca5f8c6649
amlsantos <6472330+santos-an@users.noreply.github.com>
2023-12-27 17:08:59 +0000

    refactor

--- src/Domain/StringCalculator.cs
+++ src/Domain/StringCalculator.cs
@@ -3,8 +3,17 @@ namespace Domain;
 
 public class StringCalculator
 {
-    public object Add(string v)
+    public object Add(string numbers)
     {
+        if (string.IsNullOrEmpty(numbers))
+            return 0;
+
+        if (numbers.Contains("1"))
+            return 1;
+
+        if (numbers.Contains("2"))
+            return 2;
+
         return 0;
     }
 }
--- tests/UnitTests/StringCalculatorTests.cs
+++ tests/UnitTests/StringCalculatorTests.cs
@@ -18,4 +18,45 @@ public class StringCalculatorTests
         // assert
         result.Should().Be(0);
     }
+
+    [Fact]
+    public void Add_Returns1_GivenStringWith1()
+    {
+        // arrange
+        var calculator = new StringCalculator();
+
+        // act
+        var result = calculator.Add("1");
+
+        // assert
+        result.Should().Be(1);
+    }
+
+    [Fact]
+    public void Add_Returns2_GivenStringWithOneNumber()
+    {
+        // arrange
+        var calculator = new StringCalculator();
+
+        // act
+        var result = calculator.Add("2");
+
+        // assert
+        result.Should().Be(2);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    public void Add_ReturnsNumber_GivenNumber(string input, int output)
+    {
+        // arrange
+        var calculator = new StringCalculator();
+
+        // act
+        var result = calculator.Add(input);
+
+        // assert
+        result.Should().Be(output);
+    }
 }
