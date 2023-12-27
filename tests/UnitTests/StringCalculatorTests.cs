b767108d4cb7870eb7219782813ef05441243caa
amlsantos <6472330+santos-an@users.noreply.github.com>
2023-12-27 17:42:02 +0000

    add 3 numbers test case

--- tests/UnitTests/StringCalculatorTests.cs
+++ tests/UnitTests/StringCalculatorTests.cs
@@ -8,7 +8,7 @@ public class StringCalculatorTests
 {
     private readonly StringCalculator _calculator;
 
-    public StringCalculatorTests() => 
+    public StringCalculatorTests() =>
         _calculator = new StringCalculator();
 
     [Fact]
@@ -40,7 +40,21 @@ public class StringCalculatorTests
     [InlineData("2,2", 4)]
     [InlineData("10,10", 20)]
     [InlineData("100,100", 200)]
-    public void Add_ReturnsSum_GivenNumbers(string input, int output)
+    public void Add_ReturnsSum_GivenTwoNumbers(string input, int output)
+    {
+        // act
+        var result = _calculator.Add(input);
+
+        // assert
+        result.Should().Be(output);
+    }
+
+    [Theory]
+    [InlineData("1,1,1", 3)]
+    [InlineData("2,2,2", 6)]
+    [InlineData("10,10,10", 30)]
+    [InlineData("100,100,100", 300)]
+    public void Add_ReturnsSum_GivenThreeNumbers(string input, int output)
     {
         // act
         var result = _calculator.Add(input);
