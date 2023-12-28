75d433b469d9dc048d9cfdaa8dfd00e28eb9eeb5
amlsantos <6472330+santos-an@users.noreply.github.com>
2023-12-28 15:59:12 +0000

    green - sum with custom delimeter

--- src/Domain/StringCalculator.cs
+++ src/Domain/StringCalculator.cs
@@ -11,12 +11,26 @@ public class StringCalculator
         if (numbers.Length == 1)
             return int.Parse(numbers);
 
-        var separators = new char[] {',', '\n'};
+        if (IsCustomDelimeter(numbers))
+        {
+            var customDelimeter = numbers.Substring(2, 1);
+            var customInput = numbers.Substring(4);
 
+            return customInput.Split(customDelimeter)
+                .Select(x => int.Parse(x))
+                .Sum();
+        }
+
+        var separators = new char[] { ',', '\n' };
         var sum = numbers.Split(separators)
             .Select(x => int.Parse(x))
             .Sum();
 
         return sum;
+    }
+
+    private bool IsCustomDelimeter(string numbers)
+    {
+        return numbers.StartsWith("//");
     }
 }
