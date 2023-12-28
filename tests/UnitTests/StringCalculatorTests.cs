e0bc6d2654c52fb99f4b229a067678afd34862bf
amlsantos <6472330+santos-an@users.noreply.github.com>
2023-12-28 14:44:39 +0000

    red - sum of emptyString

---
+++ tests/UnitTests/StringCalculatorTests.cs
@@ -0,0 +1,18 @@
+using Domain;
+using FluentAssertions;
+using Xunit;
+
+namespace UnitTests;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void Add_OnEmptyString_Returns0()
+    {
+        // arrange
+
+        // act
+
+        // assert
+    }
+}
